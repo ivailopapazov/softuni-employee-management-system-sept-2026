b3090f9a7d3b3269e62ef50d1ad9ff376b6e180d
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-09-21 20:18:11 +0300

    Close save user modal

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,10 @@ function App() {
         setShowSaveUserModal(true);
     };
 
+    const addUserCloseHandler = () => {
+        setShowSaveUserModal(false);
+    };
+
     return (
         <>
             <Header />
@@ -40,7 +44,7 @@ function App() {
 
                     <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button >
 
-                    {showSaveUserModal && <SaveUserModal />}
+                    {showSaveUserModal && <SaveUserModal onClose={addUserCloseHandler} />}
 
                     <Pagination />
                 </section >
