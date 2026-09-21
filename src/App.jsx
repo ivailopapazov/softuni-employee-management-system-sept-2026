5770021dee5aae37f4f379ef5fc94c40d16b0091
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-09-21 21:13:08 +0300

    Update users after post

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,11 @@ function App() {
 
     useEffect(() => {
         // Fetch users from an API or other source
-        fetch(baseUrl, {
-            headers: {
-                'apikey': apiKey,
-            }
-        })
-            .then(res => res.json())
+        fetchUsers()
             .then(data => setUsers(data))
             .catch(error => console.error('Error fetching users:', error));
     }, []);
+
 
     const addUserClickHandler = () => {
         setShowSaveUserModal(true);
@@ -35,20 +31,29 @@ function App() {
         setShowSaveUserModal(false);
     };
 
-    const submitUserHandler = (user) => {
-        // Send user to Rest API
-        fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'apikey': apiKey
-            },
-            body: JSON.stringify(user)
-        })
-            .then(() => console.log('User added:'))
-            .catch(error => alert('Error adding user: ' + error))
-            .finally(() => setShowSaveUserModal(false));
-    }
+    const submitUserHandler = async (user) => {
+        try {
+            // Send user to Rest API
+            await fetch(baseUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'apikey': apiKey
+                },
+                body: JSON.stringify(user)
+            });
+
+            // Fetch all users after adding a new one
+            const updatedUsers = await fetchUsers();
+
+            // Update the state with the latest users
+            setUsers(updatedUsers);
+        } catch (error) {
+            alert('Error adding user: ' + error);
+        } finally {
+            setShowSaveUserModal(false);
+        }
+    };
 
     return (
         <>
@@ -72,5 +77,18 @@ function App() {
         </>
     )
 }
+
+async function fetchUsers() {
+    const response = await fetch(baseUrl, {
+        headers: {
+            'apikey': apiKey,
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+}
+
 
 export default App
